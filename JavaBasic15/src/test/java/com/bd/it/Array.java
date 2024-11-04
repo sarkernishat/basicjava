c8ec579a633fac4c1af029c6013ecfeeaa8a4135
Nishat Sarker <nishatsarker.cse@gmail.com>
2024-11-04 15:55:10 +0600

    Add files via upload

---
+++ JavaBasic15/src/test/java/com/bd/it/Array.java
@@ -0,0 +1,12 @@
+package com.bd.it;
+
+public class Array {
+
+	public static void main(String[] args) {
+		String[] names = {"David", "Alex", "Nabin", "Jack", "Milan"}; //index suru hoy 0 theke
+		System.out.println("Number of Names: " +names.length);
+		System.out.println("Data of index 3: " +names[3]);
+
+	}
+
+}
